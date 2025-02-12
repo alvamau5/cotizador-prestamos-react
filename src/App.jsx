7497a0d0721544802911d41c076b2edecb64e516
alvamau5 <lucioaalvaradomolina@outlook.com>
2025-02-12 13:30:41 -0600

    add section total pay and resume pay, add useEffect callback

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header"
 import Button from "./components/Button"
-import { formatearDinero } from "./helpers"
+import { formatearDinero, calcularTotalPagar } from "./helpers"
 
 function App() {
   // Definiendo las variales con un Array destructurando el Array
   const [cantidad, setCantidad] = useState(10000);
   const [meses, setMeses] = useState(6);
+  const [total, setTotal] = useState(calcularTotalPagar(cantidad, meses));
+
+  useEffect(() => {
+    console.log('El componente esta Listo...')
+  }, []);
 
   const MIN = 0;
   const MAX = 20000;
@@ -92,6 +97,22 @@ function App() {
         <option value="12">12 meses</option>
         <option value="24">24 meses</option>
       </select>
+
+      <div className="my-5 space-y-2 bg-gray-50 p-5">
+        <h2 className="text-2xl font-extrabold text-gray-500 
+        text-center">
+          Resumen <span className="text-indigo-600">de pagos </span>
+        </h2>
+        <p className="text-xl text-gray-500 text-center font-bold">
+          {meses} Meses
+        </p>
+        <p className="text-xl text-gray-500 text-center font-bold">
+          {formatearDinero(total)} a pagar
+        </p>
+        <p className="text-xl text-gray-500 text-center font-bold">
+          Mensualidades
+        </p>
+      </div>
     </div>
   )
 }
